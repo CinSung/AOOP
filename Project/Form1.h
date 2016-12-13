0a6b78669c2da9dea54dc9d7381db6d72eaf5a78
DBlackKat <tamzhirui.eed04@nctu.edu.tw>
2016-12-13 21:45:24 +0800

    MERGE

--- Project/Form1.h
+++ Project/Form1.h
@@ -131,16 +131,9 @@ namespace Project {
 				 MarshalString(username->Text,user);
 				 PurchaseManage manager(user,pass);
 				 vector <Supplier> list = manager.getSupplierList();
-<<<<<<< HEAD
-<<<<<<< HEAD
-				 for(unsigned i=0;i < list.size();i++){
-//					name = 
-=======
+
 				 for(int i=0;i < list.size();i++){
->>>>>>> origin/master
-=======
-				 for(int i=0;i < list.size();i++){
->>>>>>> refs/remotes/origin/master
+
 					 buffer = gcnew String(list[i].getCompanyName().c_str());
 					listBox1->Items->Add(buffer);
 				 }

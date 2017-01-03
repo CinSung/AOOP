78a331a0de74698cd5c1c9260ed03a9c9eaefa8c
DBlackKat <tamzhirui.eed04@nctu.edu.tw>
2017-01-03 23:35:23 +0800

    dunno

--- Project/WarehouseChart.h
+++ Project/WarehouseChart.h
@@ -23,6 +23,7 @@ namespace Project {
 			InitializeComponent();
 			date = gcnew List<String^>();
 			amount = gcnew List<Int32>();
+			fillProduct();
 			//
 			//TODO: Add the constructor code here
 			//
@@ -57,6 +58,10 @@ namespace Project {
 	private: System::Windows::Forms::TextBox^  textBox1;
 	private: System::Windows::Forms::TextBox^  textBox2;
 	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart2;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::ComboBox^  selectBox;
 			 System::ComponentModel::Container ^components;
 	public:
 		property System::String^ companyName{
@@ -90,9 +95,12 @@ namespace Project {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^  legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^  series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea5 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend5 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series5 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea6 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^  legend6 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series6 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->name = (gcnew System::Windows::Forms::TextBox());
@@ -100,21 +108,26 @@ namespace Project {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->chart2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->selectBox = (gcnew System::Windows::Forms::ComboBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// chart1
 			// 
-			chartArea2->Name = L"ChartArea1";
-			this->chart1->ChartAreas->Add(chartArea2);
-			legend2->Name = L"Legend1";
-			this->chart1->Legends->Add(legend2);
+			chartArea5->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea5);
+			legend5->Name = L"Legend1";
+			this->chart1->Legends->Add(legend5);
 			this->chart1->Location = System::Drawing::Point(12, 41);
 			this->chart1->Name = L"chart1";
-			series2->ChartArea = L"ChartArea1";
-			series2->Legend = L"Legend1";
-			series2->Name = L"Series1";
-			this->chart1->Series->Add(series2);
+			series5->ChartArea = L"ChartArea1";
+			series5->Legend = L"Legend1";
+			series5->Name = L"Series1";
+			this->chart1->Series->Add(series5);
 			this->chart1->Size = System::Drawing::Size(408, 300);
 			this->chart1->TabIndex = 0;
 			this->chart1->Text = L"chart1";
@@ -174,11 +187,56 @@ namespace Project {
 			this->label1->Visible = false;
 			this->label1->Click += gcnew System::EventHandler(this, &WarehouseChart::label1_Click);
 			// 
+			// chart2
+			// 
+			chartArea6->Name = L"ChartArea1";
+			this->chart2->ChartAreas->Add(chartArea6);
+			legend6->Name = L"Legend1";
+			this->chart2->Legends->Add(legend6);
+			this->chart2->Location = System::Drawing::Point(426, 41);
+			this->chart2->Name = L"chart2";
+			series6->ChartArea = L"ChartArea1";
+			series6->Legend = L"Legend1";
+			series6->Name = L"Series1";
+			this->chart2->Series->Add(series6);
+			this->chart2->Size = System::Drawing::Size(347, 300);
+			this->chart2->TabIndex = 6;
+			this->chart2->Text = L"chart2";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(634, 14);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"Output";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(426, 13);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(0, 13);
+			this->label2->TabIndex = 8;
+			// 
+			// selectBox
+			// 
+			this->selectBox->FormattingEnabled = true;
+			this->selectBox->Location = System::Drawing::Point(426, 14);
+			this->selectBox->Name = L"selectBox";
+			this->selectBox->Size = System::Drawing::Size(202, 21);
+			this->selectBox->TabIndex = 9;
+			// 
 			// WarehouseChart
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(668, 369);
+			this->ClientSize = System::Drawing::Size(785, 369);
+			this->Controls->Add(this->selectBox);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->chart2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
@@ -188,11 +246,33 @@ namespace Project {
 			this->Name = L"WarehouseChart";
 			this->Text = L"WarehouseChart";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->chart2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
-		}
+		}	
 #pragma endregion
+		void fillProduct(){
+			array <Int32>^ productID;
+			array <String^>^productName;
+			array <Double>^ priceForFew;
+			string username,password;
+			MarshalString ( textBox1->Text, username );
+			MarshalString ( textBox2->Text, password );
+			MYSQL manager(username,password);
+			vector <Product> productList = manager.getProduct();
+			productID = gcnew array<Int32> (productList.size());
+			productName = gcnew array<String^> (productList.size());
+			priceForFew = gcnew array<Double> (productList.size());
+			int array_size = productList.size();
+			for(int i=0;i<productList.size();i++){
+				String^ items = gcnew String(productList[i].getName().c_str());
+				productName[i] = items;
+				productID[i] = productList[i].getID();
+				priceForFew[i] = productList[i].getPriceFew();
+				selectBox->Items->Add(items);
+			}
+		}
 		void MarshalString ( String ^ s, string& os ) {  
 			using namespace Runtime::InteropServices;  
 			const char* chars =   
